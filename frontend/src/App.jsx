9893c8ebe51b6e847243121181548925c6ec2bbc
UsmanAli90 <usmanali04768@gmail.com>
2025-01-08 01:04:02 +0500

    Added Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Navbar } from './components/Navbar';
+import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
+import Homepage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Homepage/>}></Route>
         <Route path='/signup' element={<SignUpPage/>}></Route>
