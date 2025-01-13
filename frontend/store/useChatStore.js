0fed963b5ac2de2f6036f5eef20227baed08a994
UsmanAli90 <usmanali04768@gmail.com>
2025-01-13 20:00:57 +0500

    Added HomePage components

---
+++ frontend/store/useChatStore.js
@@ -0,0 +1,35 @@
+import {create} from  'zustand';
+import toast from 'react-hot-toast';
+import { axiosInstance } from '../src/lib/axios';
+
+export const useChatStore = create((set) => ({
+    messages:[],
+    users:[],
+    selectedUser:null,
+    isUserLoading:false,
+    isMessageLoading:false,
+
+    getUsers: async()=>{
+        set({isUserLoading:true});
+        try {
+            const res=await axiosInstance.get('/message/users');
+            set({users:res.users});
+        } catch (error) {
+            toast.error('Failed to fetch users');
+        }finally{
+            set({isUserLoading:false});
+        }
+    },
+
+    getMessages: async(userID)=>{
+        set({isMessageLoading:true});
+        try {
+            const res= await axiosInstance.get(`/message/${userID}`);
+            set({messages:res.messages});
+        } catch (error) {
+            toast.error('Failed to fetch messages');
+        }finally{
+            set({isMessageLoading:false});
+        }
+    }
+}));
