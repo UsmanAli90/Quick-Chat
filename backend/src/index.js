cf5ccf5fdf5127bfdede8a1ca160bc2c0cec45c6
UsmanAli90 <usmanali04768@gmail.com>
2025-01-17 21:04:29 +0500

    Final modifications

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,22 +5,32 @@ import dotenv from 'dotenv';
 import { ConnectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import {app,server} from './lib/socket.js'
+import { app, server } from './lib/socket.js'
+import path from "path";
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: 'http://localhost:5173',
+    credentials: true
 }))
 dotenv.config();
 
-app.use('/auth',authRoutes)
-app.use('/message',messageRoutes)
+app.use('/auth', authRoutes)
+app.use('/message', messageRoutes)
 
-const port=process.env.PORT;
+const port = process.env.PORT;
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 
-server.listen(port,()=>{
-    console.log('server is running on port: '+ port)
+server.listen(port, () => {
+    console.log('server is running on port: ' + port)
     ConnectDB();
 })
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/", // Backend URL
+    baseURL: import.meta.env.MODE==="development"?"http://localhost:5001/":"/", // Backend URL
     withCredentials: true, // Send cookies when cross-origin requests
 
 })
